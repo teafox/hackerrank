be79f06b8ab14f8532a3b5acd54dcb228cd126aa
Vladimir Kolesnikov <prayforsnow@yandex.ru>
2017-06-22 05:11:16 -0700

    Python Functionals

---
+++ Python/Python Functionals/Validating_Email_Addresses_With_a_Filter.py
@@ -0,0 +1,14 @@
+import re
+
+
+def fun(s):
+    return re.match('^[a-z0-9\-_]+@[a-z0-9]+\.[a-z]{1,3}$', s)
+
+
+emails = ['its@gmail.com1',
+          'mike13445@yahoomail9.server',
+          'rase23@ha_ch.com',
+          'daniyal@gmail.coma',
+          'thatisit@thatisit',
+          ]
+print filter(fun, emails)
